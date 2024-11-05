db00fac0e18cb2e8665e71f2c223dd8d23f5e077
GianniZheng <152515879+GianniZheng@users.noreply.github.com>
2024-11-05 19:27:39 +0100

    starting

--- Pac-Man.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('ball').addEventListener('animationend', () => {
-    document.getElementById('ball').style.animation = "none";
-    document.getElementById('ball').style.animation = "moveRight 5s linear forwards";
-});
