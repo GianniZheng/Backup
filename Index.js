7bff4d674ef06481270bbbd575143f95949fa4c0
NamaTaz <152515879+GianniZheng@users.noreply.github.com>
2024-11-09 13:07:57 +0100

    theme implemented

--- Index.js
+++ Index.js
@@ -1,7 +1,9 @@
 document.querySelector(".theme-switch").addEventListener("click", () => {
     if (document.querySelector("html").dataset.theme === "light") {
       document.querySelector("html").dataset.theme = "dark";
+      document.getElementById("website-logo").src="Dark-Logo.png";
     } else {
       document.querySelector("html").dataset.theme = "light";
+      document.getElementById("website-logo").src="White-Logo.png";
     }
   });
